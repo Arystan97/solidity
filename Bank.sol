15a135cc8b668e4e3027b2e43b3b538e1083a181
Atynov Arystan <77671290+Arystan97@users.noreply.github.com>
2021-07-05 18:43:49 +0600

    Create Bank.sol

---
+++ Bank.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.8.6;
+
+contract Bank{
+    int bal;
+
+    constructor() public{
+        bal = 1;
+    }
+    
+    function getBalance() view public returns(int){
+        return bal;
+    }
+
+    function withdraw(int amt) public{
+        bal = bal - amt;
+    }
+
+    function deposit(int amt) public{
+        bal = bal + amt;
+    }
+
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true;
+    }
+}
